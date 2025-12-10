4d32357a8d870698c603c3fc1b57550356799a72
yousukeman3 <yousukeman3@gmail.com>
2025-12-10 14:42:44 +0900

    fix: resolve remaining lint errors in MapPanel and cleanup

--- src/features/character/components/sheet/ItemEditorDialog.tsx
+++ src/features/character/components/sheet/ItemEditorDialog.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-/* eslint-disable react-hooks/exhaustive-deps */
+
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
